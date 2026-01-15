9c1087aa328a886115d7de941e5626aaf3b1f79a
vichcruz <victorhugo.cruzdasilva@uzh.ch>
2026-01-15 13:51:51 +0100

    chore: Add new fields to tech-stack data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,48 +4,70 @@
 export const TechStack: {
   name: string;
   description: string;
+  familiarity: number;
+  linkToDocs: string;
 }[] = [
   {
     name: "React",
     description: "A UI library for building interactive web apps.",
+    familiarity: 8,
+    linkToDocs: "https://react.dev/",
   },
   {
     name: "Javascript",
     description: "The core language of the web.",
+    familiarity: 8,
+    linkToDocs: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     name: "Typescript",
     description: "JS with types for safer, scalable code.",
+    familiarity: 8,
+    linkToDocs: "https://www.typescriptlang.org/",
   },
   {
     name: "NestJS",
     description:
       "A progressive Node.js framework for building server-side apps.",
+    familiarity: 6,
+    linkToDocs: "https://nestjs.com/",
   },
   {
     name: "NextJS",
     description: "React framework for production with SSR and SSG.",
+    familiarity: 7,
+    linkToDocs: "https://nextjs.org/",
   },
   {
     name: "Python",
     description: "A versatile high-level programming language.",
+    familiarity: 6,
+    linkToDocs: "https://www.python.org/",
   },
   {
     name: "Java",
     description: "A class-based, object-oriented programming language.",
+    familiarity: 3,
+    linkToDocs: "https://dev.java/",
   },
   {
     name: "Django",
     description: "A high-level Python web framework for rapid development.",
+    familiarity: 4,
+    linkToDocs: "https://www.djangoproject.com/",
   },
   {
     name: "Go",
     description:
       "A statically typed language designed for simplicity and concurrency.",
+    familiarity: 4,
+    linkToDocs: "https://go.dev/",
   },
   {
     name: "Svelte",
     description:
       "A compiler that generates highly optimized JS for building UIs.",
+    familiarity: 4,
+    linkToDocs: "https://svelte.dev/",
   },
 ];
