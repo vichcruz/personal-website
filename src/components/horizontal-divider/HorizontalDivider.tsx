886b45645f581a82885bcca6604a67463f611038
vichcruz <victorhugo.cruzdasilva@uzh.ch>
2026-02-03 17:13:08 +0100

    feat: Implement HorizontalDivider component

---
+++ src/components/horizontal-divider/HorizontalDivider.tsx
@@ -0,0 +1,13 @@
+function HorizontalDivider() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "1px",
+        backgroundColor: "#000",
+      }}
+    ></div>
+  );
+}
+
+export default HorizontalDivider;
