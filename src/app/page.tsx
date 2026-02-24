925c2841ea80f425dac5b89d91c3e65daee13562
vichcruz <victorhugo.cruzdasilva@uzh.ch>
2026-02-24 17:12:48 +0100

    feat: Improve nav link behaviour
    Added hover effect to nav links and improved navigation to scroll to section centered vertically.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,24 @@ import GetToKnowMeSection from "@/components/get-to-know-me-section/GetToKnowMeS
 import IntroductionSection from "@/components/introduction-section/IntroductionSection";
 import TechStackSection from "@/components/tech-stack-section/TechStackSection";
 import Link from "next/link";
+import type { MouseEvent } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const scrollToSection =
+    (id: string) => (e: MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault();
+
+      const el = document.getElementById(id);
+      if (!el) return;
+
+      el.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest",
+      });
+    };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -16,9 +31,21 @@ export default function Home() {
         <ExperienceEducationSection />
         <GetToKnowMeSection />
         <div className={styles.navLinks}>
-          <Link href="#tech-stack">Tech Stack</Link>
-          <Link href="#experience-education">Experience & Education</Link>
-          <Link href="#get-to-know-me">Get to Know Me</Link>
+          <Link href="#tech-stack" onClick={scrollToSection("tech-stack")}>
+            Tech Stack
+          </Link>
+          <Link
+            href="#experience-education"
+            onClick={scrollToSection("experience-education")}
+          >
+            Experience & Education
+          </Link>
+          <Link
+            href="#get-to-know-me"
+            onClick={scrollToSection("get-to-know-me")}
+          >
+            Get to Know Me
+          </Link>
         </div>
       </main>
     </div>
